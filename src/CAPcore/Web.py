83e1e073b19cfec5d91f75f321530243e5afe40b
cesaralba <7543406+cesaralba@users.noreply.github.com>
2024-12-09 09:16:58 +0100

    06 remove getObjID  function from ACB (#6)
    * tests/utils/test_Misc.py: format
    
    * src/CAPcore/Web.py: removed getObjID that is too specific for ACB project
    
    ---------
    
    Co-authored-by: cap@CASA <cap@CASA>

--- src/CAPcore/Web.py
+++ src/CAPcore/Web.py
@@ -145,23 +145,6 @@ def createBrowser(config=Namespace()):
     return browser
 
 
-# https://effbot.org/zone/default-values.htm#what-to-do-instead
-sentinel = object()
-
-
-def getObjID(objURL, clave='id', defaultresult=sentinel):
-    PATid = r'^.*/' + clave + r'/(?P<id>\d+)(/.*)?'
-    REid = re.match(PATid, objURL)
-
-    if REid:
-        return REid.group('id')
-
-    if defaultresult is sentinel:
-        raise ValueError(f"getObjID '{objURL}' no casa patrón '{PATid}' para clave '{clave}'")
-
-    return defaultresult
-
-
 def findObjectsWithAttributes(webContent, targetTag, targetInfo):
     result = dict()
     for k, fname, fvalue in targetInfo:
