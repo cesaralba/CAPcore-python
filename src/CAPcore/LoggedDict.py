bd384fddb5b0d40194a00e064dc25198752fe364
cap@CASA <cap@CASA>
2024-12-19 09:03:55 +0100

    New methods and tuned behavior. Improved coverage

--- src/CAPcore/LoggedDict.py
+++ src/CAPcore/LoggedDict.py
@@ -1,7 +1,7 @@
 from time import gmtime
 
 from .LoggedValue import LoggedValue
-from typing import Dict,Any
+
 INDENTSEPR = 2
 SEPRPR = ",\n" + " " * INDENTSEPR
 
@@ -19,20 +19,22 @@ class LoggedDict:
     def __getitem__(self, item):
         return self.current.__getitem__(item).get()
 
-    def __setitem__(self, k, v):
+    def __setitem__(self, k, v,timestamp=None):
         if k in self.exclusions:
             raise KeyError(f"Key '{k}' in exclusions: {sorted(self.exclusions)}")
         currVal = self.current.get(k, LoggedValue())  # default=
-        changes = currVal.set(v)
+        changes = currVal.set(v,timestamp=timestamp)
 
         self.current[k] = currVal
         return changes
 
-    def get(self, key, default=None):
-        return self.current.get(key, default).get()
+    def get(self, key,default=None):
+        if key in self.current and not self.current[key].isDeleted():
+            return self.__getitem__(key)
+        return default
 
-    def getValue(self, key):
-        return self.current.get(key)
+    def getValue(self, key, default=None):
+        return self.current.get(key,default)
 
     def update(self, newValues, timestamp=None):
         changeTime = timestamp or gmtime()
@@ -66,8 +68,15 @@ class LoggedDict:
         return any(result)
 
     def addExclusion(self, *kargs):
+        changed=False
         self.exclusions.update(set(kargs))
-        # TOTHINK: Qué hacer con los valores almacenados y que han quedado excluidos?
+        currKeys=self.exclusions.intersection(self.current.keys())
+        for k in currKeys:
+            if k in self.exclusions:
+                self.current.pop(k)
+                changed = True
+
+        return changed
 
     def removeExclusion(self, *kargs):
         self.exclusions.remove(set(kargs).intersection(self.exclusions))
@@ -102,6 +111,27 @@ class LoggedDict:
 
         return result
 
+    def replace(self,other,timestamp=None):
+        if not isinstance(other,(dict,LoggedDict)):
+            raise TypeError(f"Parameter expected to be a dict or LoggedDict. Provided {type(other)}")
+
+        currentKeys = {k for k,v in self.current.items() if not v.isDeleted()}
+        sharedKeys = set(currentKeys).intersection(other.keys())
+        missingKeys = set(currentKeys).difference(other.keys())
+        newKeys = set(other.keys()).difference(currentKeys)
+
+        self.purge(missingKeys,timestamp=timestamp)
+
+        for k in sorted(newKeys.union(sharedKeys)):
+            if k in self.exclusions:
+                continue
+            self.__setitem__(k,other.get(k),timestamp=timestamp)
+
+    def _asdict(self):
+        result = {k:v for k,v in self.items() if not v.isDeleted()}
+        return result
+
+
     def __len__(self):
         currData = [k for k, v in self.current.items() if not v.isDeleted()]
         return len(currData)
@@ -121,129 +151,6 @@ class LoggedDict:
     def __ne__(self, other):
         return self.diff(other)
 
-
-class DictOfLoggedDict:
-    def __init__(self, exclusions: set = None):
-
-        if exclusions is not None and not isinstance(exclusions, (set, list, tuple)):
-            raise TypeError(f"DictOfLoggedDict: expected set/list/tuple for exclusions: {exclusions}")
-
-        self.current = dict()
-        self.exclusions = exclusions or set()
-        self.timestamp = gmtime()
-
-    def __getitem__(self, item):
-        return self.current.__getitem__(item)
-
-    def __setitem__(self, k, v):
-        currVal = self.current.get(k, LoggedDict(exclusions=self.exclusions))
-        changes = currVal.update(v)
-
-        self.current[k] = currVal
-        return changes
-
-    def get(self, key, default=None):
-        return self.current.get(key, default)
-
-    def update(self, newValues, timestamp=None):
-        changeTime = timestamp or gmtime()
-        result = []
-
-        if not isinstance(newValues, dict):
-            raise TypeError("LoggedDict.DictOfLoggedDict.update: expected dict")
-
-        for k, v in newValues.items():
-            currVal = self.current.get(k, LoggedDict(exclusions=self.exclusions))
-
-            r1 = currVal.update(v, timestamp=changeTime)
-
-            if r1:
-                self.current[k] = currVal
-
-            result.append(r1)
-
-        return any(result)
-
-    def purge(self, keys2delete, timestamp=None):
-        changeTime = timestamp or gmtime()
-        result = []
-
-        for k in keys2delete:
-            if k in self.exclusions:
-                continue
-            if k in self.current:
-                r1 = self.current[k].clear(timestamp=changeTime)
-                result.append(r1)
-
-        return any(result)
-
-    def addExclusion(self, *kargs):
-        self.exclusions.update(set(kargs))
-
-        for v in self.current.values():
-            v.addExclusion(set(kargs))
-        # TOTHINK: Qué hacer con los valores almacenados y que han quedado excluidos?
-
-    def removeExclusion(self, *kargs):
-        self.exclusions.remove(set(kargs).intersection(self.exclusions))
-
-        for v in self.current.values():
-            v.removeExclusion(set(kargs))
-
-    def extractKey(self, key, default=None):
-        result = {k: v.get(key, default=default) for k, v in self.current.items()}
-
-        return result
-
-    def subkeys(self):
-        auxList = []
-
-        for v in self.current.values():
-            auxList = auxList + list(v.keys())
-
-        return set(auxList)
-
-    def diff(self,other):  #:(Dict[Any,(LoggedDict,dict)],LoggedDict)
-        if not isinstance(other,(dict,DictOfLoggedDict)):
-            raise TypeError(f"Parameter expected to be a dict or DictOfLoggedDict. Provided {type(other)}")
-
-        result = DictOfLoggedDictDiff()
-
-        currentKeys = set(self.current.keys())
-        sharedKeys = set(currentKeys).intersection(other.keys())
-        missingKeys = set(currentKeys).difference(other.keys())
-        newKeys = set(other.keys()).difference(currentKeys)
-
-        for k in sorted(newKeys):
-            result.addKey(k,other.get(k))
-        for k in sorted(sharedKeys):
-            currVal = self.get(k)
-            otherVal= other.get(k)
-            result.change(k,currVal,otherVal)
-        for k in sorted(missingKeys):
-            result.removeKey(k,self.get(k))
-
-        return result
-
-    def __len__(self):
-        return len(self.current)
-
-    def __repr__(self):
-        auxResult = {k: self.current[k].__repr__() for k in sorted(self.current)}
-
-        if len(auxResult) == 1:
-            k, v = auxResult.pop()
-            result = dumpLoggedDict(k, v)
-        else:
-            claves = sorted(auxResult.keys())
-            result = "{" + " " * (INDENTSEPR - 1) + dumpLoggedDict(claves[0], auxResult[claves[0]]) + SEPRPR + " "
-            result = result + (SEPRPR + " ").join([dumpLoggedDict(k, auxResult[k]) for k in claves[1:]])
-            result = result + "\n}"
-            # TODO: WTF los espacios adicionales tras la coma
-        return result
-
-    def __ne__(self, other):
-        return self.diff(other)
 
 class LoggedDictDiff:
     def __init__(self):
@@ -275,48 +182,6 @@ class LoggedDictDiff:
         result.extend([(k,f"{k}: A '{v}'") for k,v in self.added.items()])
         result.extend([(k,f"{k}: D '{v}'") for k,v in self.removed.items()])
         result.extend([(k,f"{k}: C '{v[0]}' -> '{v[1]}'") for k,v in self.changed.items()])
-
-        auxSep = f"{sepCompact} " if compact else '\n'
-        auxIndent = 0 if compact else indent
-
-        resultStr = auxSep.join([f"{' '*((auxIndent*2)+1)}{v[1]}" for v in sorted(result,key=lambda x:x[0])])
-
-        return resultStr
-
-    def __repr__(self):
-        return self.show(compact=True)
-
-class DictOfLoggedDictDiff:
-    def __init__(self):
-        self.changeCount:int = 0
-        self.added:dict = {}
-        self.changed:dict = {}
-        self.removed:dict = {}
-
-    def change(self,k,vOld:LoggedDict,vNew):
-        diff = vOld.diff(vNew)
-        if diff:
-            self.changed[k]=diff
-            self.changeCount +=1
-
-    def addKey(self,k,vNew):
-        self.added[k]=vNew
-        self.changeCount +=1
-
-    def removeKey(self,k,vOld):
-        self.removed[k]=vOld
-        self.changeCount +=1
-
-    def __bool__(self):
-        return (self.changeCount>0)
-
-    def show(self,indent:int=0, compact:bool=False,sepCompact=','):
-        if self.changeCount == 0:
-            return ""
-        result= []
-        result.extend([(k,f"{k}: A '{v}'") for k,v in self.added.items()])
-        result.extend([(k,f"{k}: D '{v}'") for k,v in self.removed.items()])
-        result.extend([(k,f"{k}: C '{v}'") for k,v in self.changed.items()])
 
         auxSep = f"{sepCompact} " if compact else '\n'
         auxIndent = 0 if compact else indent
--- tests/utils/test_loggedDict.py
+++ tests/utils/test_loggedDict.py
@@ -9,13 +9,18 @@ class TestLoggedDict(unittest.TestCase):
         self.assertEqual(len(d1), 0)
 
     def test_constructor2(self):
+        with self.assertRaises(TypeError):
+            d1 = LoggedDict(exclusions=25)
+            d1.addExclusion('a')
+
+    def test_set1(self):
         d1 = LoggedDict(exclusions={'a'})
 
         with self.assertRaises(KeyError):
             d1['a'] = 3
 
-    def test_setget1(self):
-        d1 = LoggedDict(exclusions={'a'})
+    def test_set_get1(self):
+        d1 = LoggedDict()
 
         d1['b'] = 3
         v1 = d1['b']
@@ -23,7 +28,7 @@ class TestLoggedDict(unittest.TestCase):
         self.assertEqual(v1, 3)
         self.assertEqual(len(d1), 1)
 
-    def test_setget2(self):
+    def test_set_get2(self):
         d1 = LoggedDict(exclusions={'a'})
 
         d1['b'] = 3
@@ -35,6 +40,28 @@ class TestLoggedDict(unittest.TestCase):
         lv1 = d1.getValue('b')
         self.assertEqual(len(lv1), 2)
 
+    def test_setget1(self):
+        d1 = LoggedDict()
+
+        d1['b'] = 3
+        v1 = d1.get('b')
+        v2 = d1.get('c')
+
+        self.assertEqual(v1, 3)
+        self.assertIsNone(v2)
+
+
+    def test_setget2(self):
+        d1 = LoggedDict()
+
+        d1['b'] = 3
+        d1.purge({'b'})
+        v1 = d1.get('b','Deleted')
+        v2 = d1.get('c',25)
+
+        self.assertEqual(v1, 'Deleted')
+        self.assertEqual(v2, 25)
+
     def test_update1(self):
         data1 = {'a': 3, 'b': 4, 'c': 5}
         data2 = {'a': 4, 'b': 5, 'c': 6}
@@ -43,6 +70,13 @@ class TestLoggedDict(unittest.TestCase):
         self.assertTrue(r1)
         self.assertEqual(d1['b'], 4)
         self.assertEqual(d1['c'], 5)
+
+        r2 = d1.update(data2)
+        self.assertTrue(r2)
+        self.assertEqual(d1['b'], 5)
+        self.assertEqual(d1['c'], 6)
+        lv1=d1.getValue('b')
+        self.assertEqual(len(lv1),2)
 
     def test_update2(self):
         data1 = {'a': 3, 'b': 4, 'c': 5}
@@ -60,12 +94,35 @@ class TestLoggedDict(unittest.TestCase):
         d1 = LoggedDict(exclusions={'a'})
         d1.update(data1)
 
-        r1 = d1.purge(['c'])
+        r1 = d1.purge(['c','a'])
         self.assertTrue(r1)
 
         with self.assertRaises(ValueError):
             v1 = d1['c']
 
+
+    def test_purge2(self):
+        data1 = {'a': 3, 'b': 4, 'c': 5}
+        d1 = LoggedDict(exclusions={'a'})
+        d1.update(data1)
+
+        r1 = d1.purge(['a'])
+        self.assertFalse(r1)
+
+    def test_addExclusion1(self):
+        d1 = LoggedDict()
+        r1=d1.addExclusion('c')
+
+        self.assertEqual(len(d1.exclusions),1)
+        self.assertFalse(r1)
+
+    def test_addExclusion2(self):
+        d1 = LoggedDict()
+        d1.update({'c':25})
+        r1=d1.addExclusion('c')
+
+        self.assertEqual(len(d1),0)
+        self.assertTrue(r1)
 
 if __name__ == '__main__':
     unittest.main()
